ddbfe33743fffdec16f81a4443d2383097858c9c
Shehroz Ali <sshehroza13@gmail.com>
2021-05-04 00:27:09 +0500

    Updated style of the app

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 document.getElementById("addItem").addEventListener("click", function() {
-    let item = document.getElementById("floatingInput").value;
+    let item = document.getElementById("inputBox").value;
     
     if (item != "") {
         document.getElementById("sampleText").style.display = "none";
 
-        let listItem = document.createElement("p");
-        listItem.innerText = item;
-        document.getElementById("itemsList").appendChild(listItem);
-        document.getElementById("floatingInput").value = "";
+        let CardDiv = document.createElement("div");    // Create Main Card div
+        CardDiv.classList.add("card");
+        CardDiv.classList.add("mt-3");
+        CardDiv.classList.add("bg-transparent");
+
+        let InnerCardDiv = document.createElement("div");   // Create Inner Card div
+        InnerCardDiv.classList.add("card-body");
+        InnerCardDiv.classList.add("modifiedCard");
+        InnerCardDiv.classList.add("bg-transparent");
+        InnerCardDiv.innerText = item;      // Insert text
+
+        CardDiv.appendChild(InnerCardDiv);
+
+        document.getElementById("itemsList").appendChild(CardDiv);      // Append in the main list div
+        document.getElementById("inputBox").value = "";
     }
 })
