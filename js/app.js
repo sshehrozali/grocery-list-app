ce6d3e4d91e8feb22ac62560239259c910ea52b1
Shehroz Ali <sshehroza13@gmail.com>
2021-05-04 16:09:44 +0500

    Added ID of an element dynamically

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
+let listCounter = 0;
+
+// for (var i = 1; i < listCounter + 1; i++) {
+//     document.getElementsByClassName("itemsList")[i - 1].addEventListener("click", function() {
+//         console.log("I am ID no", i);
+//     })
+// }
+
 document.getElementById("addItem").addEventListener("click", function() {
+
     let item = document.getElementById("inputBox").value;
     
     if (item != "") {
+
+        listCounter = listCounter + 1;
+        console.log(listCounter);
+
         document.getElementById("sampleText").style.display = "none";
 
         let CardDiv = document.createElement("div");    // Create Main Card div
@@ -14,10 +27,17 @@ document.getElementById("addItem").addEventListener("click", function() {
         InnerCardDiv.classList.add("modifiedCard");
         InnerCardDiv.classList.add("bg-transparent");
         InnerCardDiv.innerText = item;      // Insert text
+        InnerCardDiv.setAttribute("id", listCounter);       // Set ID of element
 
         CardDiv.appendChild(InnerCardDiv);
 
         document.getElementById("itemsList").appendChild(CardDiv);      // Append in the main list div
         document.getElementById("inputBox").value = "";
+
+        console.log(document.getElementsByClassName("card"))
+
+        document.getElementsByClassName("card")[1].addEventListener("click", function() {
+            console.log("I am ID no", document.getElementsByClassName("card")[0].id);
+        })
     }
 })
