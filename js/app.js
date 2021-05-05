432ca9c7fb1c1a844a5c71db05163b5dbfb89436
Shehroz Ali <sshehroza13@gmail.com>
2021-05-05 12:36:23 +0500

    Added support to Local Storage

--- js/app.js
+++ js/app.js
@@ -1,43 +1,56 @@
-let listCounter = 0;
+let itemsListCounter = 0;   // Keep Counting total list items
 
-// for (var i = 1; i < listCounter + 1; i++) {
-//     document.getElementsByClassName("itemsList")[i - 1].addEventListener("click", function() {
-//         console.log("I am ID no", i);
-//     })
-// }
+// Recall all data from Local Storage once the page is loaded
+document.addEventListener("DOMContentLoaded", function() {
+    
+    if (localStorage.length != 0) {
+        document.getElementById("sampleText").style.display = "none";
+    }
 
+    for (var i = 1; i < localStorage.length + 1; i++) {
+        itemsListCounter = localStorage.length;
+
+        storedItem = localStorage.getItem(i);
+        makeListItem(storedItem);
+    }
+  });
+
+// Add Item
 document.getElementById("addItem").addEventListener("click", function() {
 
     let item = document.getElementById("inputBox").value;
     
     if (item != "") {
 
-        listCounter = listCounter + 1;
-        console.log(listCounter);
+        itemsListCounter = itemsListCounter + 1;
+
+        localStorage.setItem(itemsListCounter, item);   // Store in Local Storage
+
+        // Call function
+        makeListItem(item);
 
         document.getElementById("sampleText").style.display = "none";
-
-        let CardDiv = document.createElement("div");    // Create Main Card div
-        CardDiv.classList.add("card");
-        CardDiv.classList.add("mt-3");
-        CardDiv.classList.add("bg-transparent");
-
-        let InnerCardDiv = document.createElement("div");   // Create Inner Card div
-        InnerCardDiv.classList.add("card-body");
-        InnerCardDiv.classList.add("modifiedCard");
-        InnerCardDiv.classList.add("bg-transparent");
-        InnerCardDiv.innerText = item;      // Insert text
-        InnerCardDiv.setAttribute("id", listCounter);       // Set ID of element
-
-        CardDiv.appendChild(InnerCardDiv);
-
-        document.getElementById("itemsList").appendChild(CardDiv);      // Append in the main list div
         document.getElementById("inputBox").value = "";
 
-        console.log(document.getElementsByClassName("card"))
-
-        document.getElementsByClassName("card")[1].addEventListener("click", function() {
-            console.log("I am ID no", document.getElementsByClassName("card")[0].id);
-        })
     }
-})
+});
+
+// Function to make list item
+function makeListItem(item) {
+    
+    let CardDiv = document.createElement("div");    // Create Main Card div
+    CardDiv.classList.add("card");
+    CardDiv.classList.add("mt-3");
+    CardDiv.classList.add("bg-transparent");
+
+    let InnerCardDiv = document.createElement("div");   // Create Inner Card div
+    InnerCardDiv.classList.add("card-body");
+    InnerCardDiv.classList.add("modifiedCard");
+    InnerCardDiv.classList.add("bg-transparent");
+    InnerCardDiv.innerText = item;      // Insert text
+    InnerCardDiv.setAttribute("id", itemsListCounter);       // Set ID of element
+
+    CardDiv.appendChild(InnerCardDiv);
+
+    document.getElementById("itemsList").appendChild(CardDiv);      // Append in the main list div
+}
